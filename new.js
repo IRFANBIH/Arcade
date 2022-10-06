e571994112c344b1793a88576b015f2b37fc9570
irfan <ipekusic@yahoo.com>
2022-10-06 00:33:08 -0400

    whole new project but way better understanding.

---
+++ new.js
@@ -0,0 +1,15 @@
+let X_class='x' //James Robinson // x as x's
+let Y_class='y' // Travis Etienne // y as o's
+
+
+/** selector */
+
+function characterSelector(){
+    let selectedCharacter = document.querySelectorAll(".startGame .img .id");
+    selectedCharacter.forEach(img=>{
+        console.log(selectedCharacter);
+    });
+
+} 
+
+characterSelector()
